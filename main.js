061932a05e8eb9d854eac8fca8284759e65d3747
Sudarshan Malla <48778320+sudarshanm03986@users.noreply.github.com>
2023-09-06 23:50:38 +1200

    Initial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+import './style.css'
+
+// scene camera render
+import * as THREE from 'three';
+
+//allow the mouse to move around
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
+//scence
+const scene = new THREE.Scene();
+
+//camera 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//render
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+// Shape Tours
+const geometry = new THREE.TorusGeometry(10,3,16,100)
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347 });
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus)
+
+// pointing light
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+// ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+// light helper
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+
+//mouse move
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100) );
+
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+
+//Animation loop
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
